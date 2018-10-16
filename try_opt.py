1766ecf45ec2f1cb4be8755e3ccb9ea88b3e1098
monicamazur <monica.mazur9@gmail.com>
2018-10-16 00:48:28 -0400

    Updated

--- try_opt.py
+++ try_opt.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy import optimize
-
+from pandas import Series
+from sklearn.preprocessing import MinMaxScaler
 from App import GuesserTemplate, prm_mtrx_to_dict
 
 
@@ -10,6 +11,7 @@ class OptimiseGuesser(GuesserTemplate):
         # guess the curve parameters and save them, in same structure as "params" from Generator, in self.params
 
         # IMPLEMENT Scipy Optimize
+
         # This cannot be done in a for-loop. You have to optimise all parameters at the same time! :)
 
         # I will implement this for a general case with fixed starting guesses as an example.
@@ -23,8 +25,9 @@ class OptimiseGuesser(GuesserTemplate):
 
         # here I am starting all parameter guesses at 0. Try to loop over different start values
         x0 = np.zeros(self.n_params())
+        x0 = np.array([range([1,len(params[shp],1)])
 
-        best_fit_params = optimize.minimize(f, x0, method=method)['x']
+        best_fit_params = optimize.minimize(self, x0, method=method)['x'])
 
         # Todo: **please understand very carefully here how I have used SciPy's function (without copying anything
         # todo: from SciPy internal entrails) and how each argument of the single call to a SciPy function corresponds
@@ -37,3 +40,18 @@ class OptimiseGuesser(GuesserTemplate):
         # todo: 1) Write a function to normalise the input data, and try to set reasonable bounds for all
         # todo: (cont.) shp parameters for a normalised input data case
         # todo: 2) Then try implementing different starting points and different optimisation algorithms
+
+        values = self.series.values
+        values = self.series.reshape((len(values),1))
+
+        # train the normalization
+        scaler = MinMaxScaler(feature_rangeg=(0,1))
+        scaler = scaler.fit(values)
+        normalized = scaler.transform(values)
+
+        lims_params = {
+                'logistic': {'a': (-10,10), 'b': (-10,10), 'c': (0,-20)},
+                'exponential': {'a': (1, 10), 'b': (-1, 1)},
+
+
+        best_fit_params = optimize.minimize(self, x0, method=method)['x']
